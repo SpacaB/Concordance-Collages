4eb84d1ddf978c5da0624b52a14ae4a6d669676e
Stephen Bishop <SpacaB@users.noreply.github.com>
2015-05-01 18:19:18 -0400

    Update makecollage.py

--- makecollage.py
+++ makecollage.py
@@ -10,6 +10,7 @@ Created on Fri May 01 16:29:25 2015
 from collage import preprocess as make
 
 # open the text file of the speech we'll be using
+# make sure the file you call is in your working directory
 storytext = open("DrumMajor.txt").read()
 
 # run this line of code to spit out everything we need
